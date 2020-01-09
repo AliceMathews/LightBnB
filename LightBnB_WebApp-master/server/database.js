f8fe525b2270bb1a080b90eef2fb7d87de4ae68e
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-09 22:17:46 +0000

    updated get all properties function to allow query to include search results

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -135,10 +135,57 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  let queryString = `SELECT * FROM properties LIMIT $1`
+
+  const queryParams = [];
+
+  let queryString = `
+    SELECT properties.*, avg(rating) as average_rating 
+    FROM properties
+    LEFT OUTER JOIN property_reviews ON property_id = properties.id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+
+    if (queryParams.length > 1) { 
+      queryString += ` AND properties.cost_per_night <= $${queryParams.length}`
+    } else {
+      queryString += `WHERE properties.cost_per_night <= $${queryParams.length}`
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+
+    if (queryParams.length > 1) { 
+      queryString += ` AND properties.cost_per_night >= $${queryParams.length}`
+    } else {
+      queryString += `WHERE properties.cost_per_night >= $${queryParams.length}`
+    }
+  }
+
+  queryString += `
+    GROUP BY properties.id
+  `
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(rating) >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
 
   return pool
-    .query(queryString, [limit])
+    .query(queryString, queryParams)
     .then(res => res.rows)
     .catch(err => {
       console.error('query error', err.stack)
